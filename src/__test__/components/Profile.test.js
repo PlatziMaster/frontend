4b9f4443dba2b22493e027b393f470c646188c42
Oscar Barajas <oscar@dragons.mx>
2021-04-22 08:56:26 -0500

    :art: [ADD] Lock S-foils in attack position

---
+++ src/__test__/components/Profile.test.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { mount } from 'enzyme';
+import Profile from '../../components/Profile';
+
+describe('<Profile />', () => {
+  const profile = mount(<Profile />);
+
+  test('Profile render', () => {
+    expect(profile.length).toEqual(1);
+  });
+
+  test('Profile title', () => {
+    expect(profile.find('.Profile-title').length).toEqual(1);
+  });
+
+  test('Profile have a description', () => {
+    expect(profile.find('.Profile-desc').length).toEqual(1);
+  });
+
+});

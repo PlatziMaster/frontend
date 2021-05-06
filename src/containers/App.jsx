f415999248a59bfda2b695f3d9e2fa7e6e597f0d
Raul Camacho <osvaldoracm@gmail.com>
2021-05-06 16:06:32 -0500

    integracion de API

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import '../styles/components/App.styl';
 import Header from '../components/Header';
 import About from '../components/About';
@@ -8,21 +8,40 @@ import Academic from '../components/Academic';
 import Skills from '../components/Skills';
 import Interest from '../components/Interest';
 import Languages from '../components/Languages';
+import getData from '../utils/getData';
 
 const App = () => {
+  const [data, setdata] = useState(null);
+
+  useEffect(() => {
+    let req = getData('../../data.json ');
+    req.then(res => setdata(res.data));
+  }, []);
+
+  if (data === null) {
+    return null;
+  }
   return (
-    <>
-      <Header>
-        <About />
+    <main className="Content">
+      <Header
+        avatar={data.avatar}
+        name={data.name}
+        profession={data.profession}
+        socialMedia={data.social}
+      >
+        <About data={data} />
       </Header>
-      <Profile />
-      <Experience />
-      <Academic />
-      <Skills />
-      <Interest />
-      <Languages />
-    </>
-  )
+      <Profile description={data.Profile} />
+      <Experience experience={data.experience} />
+
+      <div className="Professional-content">
+        <Academic academic={data.Academic} />
+        <Skills skills={data.skills} />
+        <Interest interest={data.interest} />
+        <Languages languages={data.languages} />
+      </div>
+    </main>
+  );
 };
 
 export default App;

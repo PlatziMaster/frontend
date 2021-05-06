bd6c98f77eda91df48b2ddc1bc33dc923b558ec4
Fernando De Leon <gasnare@hotmail.com>
2021-05-06 00:04:12 -0500

    New components =

---
+++ src/components/About.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const About = () => {
+  return (
+    <div className='Profile-title main'>
+      <p>
+        My name is
+        <strong> Fernando De Leon</strong>
+        . I am a 28 year old Colombian frontend developer who cares about design. I am a Technologist from the Universidad San Buenaventura Bogotá. Design web pages and mobile applications for Android and IOS in flutter. Lover of programming and music, in addition to playing instruments such as guitar and drums.
+      </p>
+    </div>
+  );
+};
+
+export default About;
---
+++ src/components/Experience.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Experience = () => {
+  return (
+    <div className='Experience main'>
+      <p>Prieba</p>
+    </div>
+  );
+};
+
+export default Experience;

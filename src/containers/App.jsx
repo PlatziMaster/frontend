5645d74e6809c71cbd689339522aca1eddcc82c7
Fernando De Leon <gasnare@hotmail.com>
2021-05-06 00:01:55 -0500

    Implement the new components

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,28 +1,28 @@
 import React from 'react';
-import '../styles/components/App.styl';
+import '../styles/components/App.css';
 import Header from '../components/Header';
 import About from '../components/About';
-import Profile from '../components/Profile';
+// import Profile from '../components/Profile';
 import Experience from '../components/Experience';
-import Academic from '../components/Academic';
-import Skills from '../components/Skills';
-import Interest from '../components/Interest';
-import Languages from '../components/Languages';
+// import Academic from '../components/Academic';
+// import Skills from '../components/Skills';
+// import Interest from '../components/Interest';
+// import Languages from '../components/Languages';
 
 const App = () => {
   return (
     <>
-      <Header>
-        <About />
-      </Header>
-      <Profile />
+      <Header />
+      <About />
       <Experience />
+      {/*
+      <Profile />
       <Academic />
       <Skills />
       <Interest />
-      <Languages />
+      <Languages /> */}
     </>
-  )
+  );
 };
 
 export default App;

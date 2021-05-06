078727be3291a6f1bec561d6d3927ff3747cb7af
Raul Camacho <osvaldoracm@gmail.com>
2021-05-06 18:25:38 -0500

    documentacion con prop-types

--- src/__test__/components/Header.test.js
+++ src/__test__/components/Header.test.js
@@ -1,9 +1,17 @@
 import React from 'react';
 import { mount } from 'enzyme';
 import Header from '../../components/Header';
+import data from '../../../data.json';
 
 describe('<Header />', () => {
-  const header = mount(<Header />);
+  const header = mount(
+    <Header
+      avatar={data.data.avatar}
+      name={data.data.name}
+      profession={data.data.profession}
+      socialMedia={data.data.social}
+    />
+  );
 
   test('Header render', () => {
     expect(header.length).toEqual(1);
@@ -12,5 +20,4 @@ describe('<Header />', () => {
   test('Header title', () => {
     expect(header.find('.Header-title').length).toEqual(1);
   });
-
 });

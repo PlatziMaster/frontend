614e03c9c301379544e2dc26f83ad0432c8e213b
Fernando De Leon <gasnare@hotmail.com>
2021-05-06 18:30:04 -0500

    Create footer of web site

---
+++ src/components/Footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <div className='Footer-container'>
+      <p>
+        Fernando
+        <span> De Leon</span>
+      </p>
+    </div>
+  );
+};
+
+export default Footer;

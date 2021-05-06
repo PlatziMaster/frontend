1fad79f8badfe19cba1dcb60533b67c8b6d3a46c
Fernando De Leon <gasnare@hotmail.com>
2021-05-06 17:53:21 -0500

    Create component Academic

---
+++ src/components/Academic.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+const Academic = () => {
+  return (
+    <div className='Profile-container main'>
+      <h2 className='Profile-title'>
+        Universidad San Buenaventura – Tecnología en Programación y Desarrollo de Software
+      </h2>
+      <p>
+        I finished my studies as a Technologist in programming and software development at the
+        Universidad de San Buenaventura Bogotá, learning Java programming languages,
+        SQL databases, programming logic, algorithms.
+        First semesters emphasis on programming language in Java, databases in
+        MySQL and PL / SQL, with strong project implementation learning,
+        methodologies, strong bases in documentation and the experience of implementing a
+        functional project.
+        Learning about development and programming of web pages in languages such as PHP and
+        JavaScript.
+        In 2019 studies were started for mobile applications on Android, taking the
+        learned during 4 semesters in the Java language and database, to create
+        native Android applications fully functional and continuing learning
+        to this day.
+      </p>
+    </div>
+  );
+};
+
+export default Academic;

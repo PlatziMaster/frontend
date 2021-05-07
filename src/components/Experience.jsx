1cbced46d1d05a8af528abeae41c1dd9f9e06d9c
Fernando De Leon <gasnare@hotmail.com>
2021-05-06 20:04:04 -0500

    Implementen de api in component

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,9 +1,36 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 const Experience = () => {
+
+  const [dataInfo, setDataInfo] = useState([]);
+
+  useEffect(() => {
+    fetch('http://localhost:3000/data')
+      .then(response => response.json())
+      .then(data => setDataInfo(data.experience));
+  }, []);
+
   return (
     <div className='Experience main'>
-      <p>Prieba</p>
+      <h2 className='Experience-title'>
+        Work experience
+      </h2>
+      <div className='Experience-container'>
+        {dataInfo.map(data => (
+          <div className='Experience-item'>
+            <h2>
+              {data.startDate}
+              {' - '}
+              {data.endDate}
+            </h2>
+            <h3>{data.company}</h3>
+            <h3>{data.jobTitle}</h3>
+            <p>
+              {data.jobDescription}
+            </p>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };

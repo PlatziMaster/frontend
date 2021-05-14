92f450b9f949438ef4ceaafc892020ceca4e9350
Nicolas-alt <a10100105555@gmail.com>
2021-05-14 10:32:00 -0500

    Update(Dependencies): Upgrade [Bonus] #7

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
+import {
+  Header,
+  About,
+  Profile,
+  Experience,
+  Academic,
+  Skills,
+  Interest,
+  Languages,
+} from '../components';
 import '../styles/components/App.styl';
-import Header from '../components/Header';
-import About from '../components/About';
-import Profile from '../components/Profile';
-import Experience from '../components/Experience';
-import Academic from '../components/Academic';
-import Skills from '../components/Skills';
-import Interest from '../components/Interest';
-import Languages from '../components/Languages';
 
 const App = () => {
   return (

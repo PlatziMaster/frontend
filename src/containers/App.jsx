fb65b341dcb70f239914065f36140a249caf3c52
DESKTOP-4118O6M\zanar <imcrisam@gmai..com>
2021-05-15 21:34:12 -0500

    intento de grid

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,5 +1,5 @@
-import React, {useState, useEffect } from 'react';
-import '../styles/components/App.styl';
+import React, { useState, useEffect } from 'react';
+
 import Header from '../components/Header';
 import About from '../components/About';
 import Profile from '../components/Profile';
@@ -11,29 +11,53 @@ import Languages from '../components/Languages';
 
 import getData from '../utils/getData.js';
 
-const App = () => {
+import { Container,Row,Col } from 'react-bootstrap';
 
+
+const App = () => {
   const [Info, setInfo] = useState({});
 
   useEffect(() => {
-    getData('http://localhost:3000/data').then((resp) => {
-      setInfo(resp)
+    getData('http://localhost:3000/data').then(resp => {
+      setInfo(resp);
     });
-  },[])
-  
+  }, []);
+
   return (
-    <>
-      <Header {...Info}>
-        <About {...Info}/>
-      </Header>
-      <Profile {...Info}/>
-      <Experience {...Info}/>
-      <Academic {...Info}/>
-      <Skills {...Info}/>
-      <Interest {...Info}/>
-      <Languages {...Info}/>
-    </>
-  )
+    <Container fluid >
+      <Row class="justify-content-md-center" display>
+        <Header {...Info}>
+          <About {...Info} />
+        </Header>
+      </Row>
+
+      <Row>
+        <Profile {...Info} />
+      </Row>
+
+      <Row>
+        <Experience {...Info} />
+      </Row>
+
+      <Row>
+        <Col xs={12} md={6}>
+          <Academic {...Info} />
+        </Col>
+        <Col xs={12} md={6}>
+          <Skills {...Info} />
+        </Col>
+      </Row>
+
+      <Row>
+        <Col xs={12} md={6}>
+          <Interest {...Info} />
+        </Col>
+        <Col xs={12} md={6} className="d-flex">
+          <Languages {...Info} />
+        </Col>
+      </Row>
+    </Container>
+  );
 };
 
 export default App;

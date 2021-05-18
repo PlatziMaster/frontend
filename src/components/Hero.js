f5742e37c4bc26bc049e3db46bfd8e78b80bd460
jovenesD <emo30000@gmail.com>
2021-05-18 14:44:29 -0500

    [feat ] Components: Created component Hero.

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,5 +1,13 @@
 import React from 'react';
+import '../styles/components/Hero.styl';
 
 export default function Hero() {
-  return <div className="Hero">Hero</div>;
+  return (
+    <div className="Hero">
+      <div className="Hero__name">Guillermo Loza</div>
+      <div className="Hero__title">title</div>
+      <div className="Hero__description">description</div>
+      <div className="Hero__description">description</div>
+    </div>
+  );
 }
--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import '../styles/components/App.styl';
 
+import Hero from '../components/Hero';
+
 const App = () => {
   return (
     <>
-      <div className="hello">Hello world</div>
+      <Hero />
     </>
   );
 };

2374549b30a1fb87f355cc2c319c76e452443988
jovenesD <emo30000@gmail.com>
2021-05-18 16:35:26 -0500

    [feat] server: The data base has been updated and configured.

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import '../styles/components/Hero.styl';
 
-export default function Hero() {
+export default function Hero({ title, name, description }) {
   return (
     <div className="Hero">
-      <div className="Hero__name">Guillermo Loza</div>
-      <div className="Hero__title">title</div>
-      <div className="Hero__description">description</div>
-      <div className="Hero__description">description</div>
+      <div className="Hero__name">{name}</div>
+      <div className="Hero__title">{title}</div>
+      <div className="Hero__description">{description}</div>
     </div>
   );
 }
--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,14 +1,34 @@
-import React from 'react';
-import '../styles/components/App.styl';
-
+import React, { useEffect, useState } from 'react';
+import fetchData from '../utils/fetchData';
+//Components
 import Hero from '../components/Hero';
 
+//Styles
+import '../styles/components/App.styl';
+
 const App = () => {
+  const [data, setData] = useState({});
+  useEffect(() => {
+    // eslint-disable-next-line arrow-parens
+    fetchData('http://localhost:3000/data').then(collectedData => {
+      setData(collectedData);
+    });
+  }, []);
+
+  console.log(data);
+
   return (
     <>
-      <Hero />
+      <div>
+        {data && (
+          <Hero
+            title={data.title}
+            name={data.name}
+            description={data.profile}
+          />
+        )}
+      </div>
     </>
   );
 };
-
 export default App;

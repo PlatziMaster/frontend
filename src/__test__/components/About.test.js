36a548c528283a06f47ddf1c752be632d8a232ee
Felipe Duitama <felipedc09@gmail.com>
2021-05-19 01:42:37 -0500

    update tests

--- src/__test__/components/About.test.js
+++ src/__test__/components/About.test.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import { mount } from 'enzyme';
 import About from '../../components/About';
+import { data } from '../../../data.json';
 
 describe('<About />', () => {
-  const about = mount(<About />);
+  const about = mount(<About data={data} />);
 
   test('About render', () => {
     expect(about.length).toEqual(1);

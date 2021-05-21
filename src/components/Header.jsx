20c349a4d046ed533029f6cd16672f0b3708969d
juan <jpardila38@misena.edu.co>
2021-05-20 21:10:48 -0500

    API consumption, main styles, Header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import '../styles/components/Header.styl';
+
+const Header = () =>{
+
+    return(
+        <div className="container">
+            <div className="header">
+                <h1 className="titulo">Header</h1>
+            </div>
+        </div>
+    )
+}
+
+export default Header;
--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,18 +1,31 @@
-import React from 'react';
-import '../styles/components/App.styl';
+import React, { useState, useEffect } from 'react';
+import getData from '../utils/getData';
+import '../styles/App.styl';
 import Header from '../components/Header';
-import About from '../components/About';
-import Profile from '../components/Profile';
-import Experience from '../components/Experience';
-import Academic from '../components/Academic';
-import Skills from '../components/Skills';
-import Interest from '../components/Interest';
-import Languages from '../components/Languages';
+// import About from '../components/About';
+// import Profile from '../components/Profile';
+// import Experience from '../components/Experience';
+// import Academic from '../components/Academic';
+// import Skills from '../components/Skills';
+// import Interest from '../components/Interest';
+// import Languages from '../components/Languages';
 
 const App = () => {
+  const api = 'http://localhost:3000/data'
+  const [data, setData] = useState([])
+
+  useEffect(() => {
+    getData(api)
+      .then(response => setData(response))
+      .catch(error => console.error(error))
+  },[])
+
+  console.log(data)
+
   return (
     <>
-      <Header>
+    <Header />
+      {/* <Header>
         <About />
       </Header>
       <Profile />
@@ -20,7 +33,7 @@ const App = () => {
       <Academic />
       <Skills />
       <Interest />
-      <Languages />
+      <Languages /> */}
     </>
   )
 };

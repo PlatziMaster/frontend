98135bc342a763d61ea97472a27e8dc889aaa7b9
juan <jpardila38@misena.edu.co>
2021-05-20 21:11:04 -0500

    API consumption, main styles, Header component

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -2,13 +2,7 @@ import React, { useState, useEffect } from 'react';
 import getData from '../utils/getData';
 import '../styles/App.styl';
 import Header from '../components/Header';
-// import About from '../components/About';
-// import Profile from '../components/Profile';
-// import Experience from '../components/Experience';
-// import Academic from '../components/Academic';
-// import Skills from '../components/Skills';
-// import Interest from '../components/Interest';
-// import Languages from '../components/Languages';
+
 
 const App = () => {
   const api = 'http://localhost:3000/data'
@@ -25,15 +19,6 @@ const App = () => {
   return (
     <>
     <Header />
-      {/* <Header>
-        <About />
-      </Header>
-      <Profile />
-      <Experience />
-      <Academic />
-      <Skills />
-      <Interest />
-      <Languages /> */}
     </>
   )
 };

425cb3b1491b4db38eea6b0d7a1f4b01c8f2582b
Vanessa Alonso <vpalonsog@gmail.com>
2021-06-06 17:55:27 -0500

    Add build into project

---
+++ build/index.js
@@ -0,0 +1,11 @@
+console.log( `Hola Platzi Team. Es un gusto para mi estar aquí,
+pero aún no tengo conocimiento en React Js.
+Sin embargo, me gustaría mucho ser parte de la comunidad 🥺💚`);
+
+
+setTimeout(() => {
+  alert(`Hola Platzi Team. Es un gusto para mi estar aquí,
+  pero aún no tengo conocimiento en React Js.
+  Sin embargo, me gustaría mucho ser parte de la comunidad 🥺💚`);
+},3000)
+

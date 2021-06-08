e38450b2ea2244bfd42761608a18f5a3c3cc8873
altguerrero <josealberto.guerreroayala@gmail.com>
2021-06-07 23:42:52 -0500

    getdata function created

---
+++ src/utils/getData.js
@@ -0,0 +1,9 @@
+export const getData = async (url) => {
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error(error);
+  }
+};

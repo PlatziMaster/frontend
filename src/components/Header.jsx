6908dd07fdc77d654191d2d26bf543accf5b5001
Jorge Trad <jetradl@gmail.com>
2021-07-27 12:28:15 -0500

    Adding the contact and social media section in the Header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import '../styles/components/Header.styl';
 import Gravatar from './Gravatar';
 import Title from './Title';
+import Subtitle from './Subtitle';
 
 const Header = () => (
   <div className="Header">
@@ -14,14 +15,28 @@ const Header = () => (
       </div>
     </div>
     <div className="Header__info">
-      <h2>Jorge Elias Trad López</h2>
+      <h2>Jorge Trad</h2>
       <h3>Frontend Development Student </h3>
-      <div className="Header__info-contact">
-        <p>9992098495</p>
-        <p>jetradl@gmail.com</p>
-        <p>platzi.com/p/jorgetrad/</p>
-      </div>
       <p>Mérida, Yucatán, México</p>
+      <div className="Header__infoContainer">
+        <div className="Header__info-contact">
+          <Subtitle title="Contacts"></Subtitle>
+          <div className="Header__info-contactElements">
+            <p>9992098495</p>
+            <p>jetradl@gmail.com</p>
+            <a href="https://platzi.com/p/jorgetrad/">Website</a>
+          </div>
+        </div>
+        <div className="Header__info-social">
+          <Subtitle title="Social"></Subtitle>
+          <div className="Header__info-socialLinks">
+            <a href="https://www.facebook.com/jorgeelias.tradlopez">Facebook</a>
+            <a href="https://twitter.com/jorge_trad">Twitter</a>
+            <a href="https://github.com/jorgetrad99">Github</a>
+            <a href="https://www.linkedin.com/in/jorgetrad/">LinkedIn</a>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 );

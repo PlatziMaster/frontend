94ea9d9b53ff5f320d9caccfeb875dcee1dcdbfe
Jorge Trad <jetradl@gmail.com>
2021-07-27 20:07:40 -0500

    Adding icons to the contact and social sections on Header element

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,6 +4,15 @@ import Gravatar from './Gravatar';
 import Title from './Title';
 import Subtitle from './Subtitle';
 
+//Icons
+import Phone from '../assets/icons/phone.svg';
+import Email from '../assets/icons/mail.svg';
+import Website from '../assets/icons/software-engineer.svg';
+import Facebook from '../assets/icons/facebook-round.svg';
+import Twitter from '../assets/icons/twitter-round.svg';
+import Github from '../assets/icons/github.svg';
+import LinkedIn from '../assets/icons/linkedin-round.svg';
+
 const Header = () => (
   <div className="Header">
     <div className="Header__title-avatar">
@@ -20,20 +29,41 @@ const Header = () => (
       <p>Mérida, Yucatán, México</p>
       <div className="Header__infoContainer">
         <div className="Header__info-contact">
-          <Subtitle title="Contacts"></Subtitle>
+          <Subtitle title="Contacts" />
           <div className="Header__info-contactElements">
-            <p>9992098495</p>
-            <p>jetradl@gmail.com</p>
-            <a href="https://platzi.com/p/jorgetrad/">Website</a>
+            <div>
+              <img src={Phone} alt="Phone icon" />
+              <p>9992098495</p>
+            </div>
+            <div>
+              <img src={Email} alt="Email icon" />
+              <p>jetradl@gmail.com</p>
+            </div>
+            <div>
+              <img src={Website} alt="Website icon" />
+              <a href="https://platzi.com/p/jorgetrad/">Website</a>
+            </div>
           </div>
         </div>
         <div className="Header__info-social">
           <Subtitle title="Social"></Subtitle>
           <div className="Header__info-socialLinks">
-            <a href="https://www.facebook.com/jorgeelias.tradlopez">Facebook</a>
-            <a href="https://twitter.com/jorge_trad">Twitter</a>
-            <a href="https://github.com/jorgetrad99">Github</a>
-            <a href="https://www.linkedin.com/in/jorgetrad/">LinkedIn</a>
+            <div>
+              <img src={Facebook} alt="" />
+              <a href="https://www.facebook.com/jorgeelias.tradlopez">Jorge Trad</a>
+            </div>
+            <div>
+              <img src={Twitter} alt="" />
+              <a href="https://twitter.-roundjorge_trad">jorge_trad</a>
+            </div>
+            <div>
+              <img src={Github} alt="" />
+              <a href="https://github.com/jorgetrad99">jorgetrad99</a>
+            </div>
+            <div>
+              <img src={LinkedIn} alt="" />
+              <a href="https://www.linkedin.com/in/jorgetrad/">jorgetrad</a>
+            </div>
           </div>
         </div>
       </div>

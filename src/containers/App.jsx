7a567921fa3bd697c114b6f8c27400dc5713ca88
Mariana Hernández <72678409+themattfire@users.noreply.github.com>
2021-07-30 12:54:15 -0500

    Setting up components on main file

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import '../styles/components/App.styl';
 import Header from '../components/Header';
 import About from '../components/About';
@@ -8,21 +8,38 @@ import Academic from '../components/Academic';
 import Skills from '../components/Skills';
 import Interest from '../components/Interest';
 import Languages from '../components/Languages';
+import getData from '../utils/getData';
 
 const App = () => {
+  const [data, setData] = useState({});
+
+  useEffect(() => {
+    getData('http://localhost:3000/data').then(setData);
+  }, []);
+
   return (
     <>
-      <Header>
-        <About />
+      <Header avatar={data.avatar} name={data.name}>
+        <About
+          profession={data.profession}
+          address={data.address}
+          email={data.email}
+          website={data.website}
+          phone={data.phone}
+        />
       </Header>
-      <Profile />
-      <Experience />
-      <Academic />
-      <Skills />
-      <Interest />
-      <Languages />
+      <Profile prof={data.profile} />
+      <Experience experience={data.experience} projects={data.projects} />
+      <div className='habs__container'>
+        <Academic academic={data.academic} certificate={data.certificate} />
+        <Skills softskills={data.softskills} hardskills={data.hardskills} />
+      </div>
+      <div className='int__container'>
+        <Interest interest={data.interest} />
+        <Languages languages={data.languages} />
+      </div>
     </>
-  )
+  );
 };
 
 export default App;

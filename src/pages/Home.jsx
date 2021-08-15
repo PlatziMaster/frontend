bc5105773b2eed26e081086dcc6705455fbdf052
Irving Juarez <irvingjuarez274@gmail.com>
2021-08-14 19:56:16 -0500

    The home page is almost done, left to do in that page is the cards for the projects

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,55 @@
 import React from "react"
+import Section from "../components/Section"
+
+import "../styles/pages/Home.css"
 
 const Home = () => (
-    <h2>Home</h2>
+  <React.Fragment>
+    <Section title="Personal Profile">
+      <p>
+        I am a front-end web developer with knowledge about Git, Github and the terminal. Besides that, I have a high level of mastery in html, css and JavaScript. Currently I develop all my projects with React.js
+      </p>
+    </Section>
+
+    <Section title="Skills">
+      <div className="skills-content">
+        <ul>
+          <li>
+            <span>Prompt</span>
+          </li>
+          <li>
+            <span>Git & GitHub</span>
+          </li>
+          <li>
+            <span>Html</span>
+          </li>
+          <li>
+            <span>Css</span>
+          </li>
+          <li>
+            <span>Vanilla JavaScript</span>
+          </li>
+          <li>
+            <span>React.js</span>
+          </li>
+          <li>
+            <span>Webpack</span>
+          </li>
+          <li>
+            <span>React Router</span>
+          </li>
+        </ul>
+      </div>
+    </Section>
+
+    <Section title="Experience">
+      <ul>
+        <li>Card</li>
+        <li>Card</li>
+        <li>Card</li>
+      </ul>
+    </Section>
+  </React.Fragment>
 )
 
 export default Home

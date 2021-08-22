a5f0a0fbb0084662c3e6032c29b157c90e0a3889
Daniel Sanchez <danielsanchez1502@gmail.com>
2021-08-22 16:27:56 -0400

    Ajustes a Webpack

--- webpack.config.js
+++ webpack.config.js
@@ -11,10 +11,6 @@ module.exports = {
   },
   resolve: {
     extensions: ['.js', '.jsx'],
-    alias: {
-      '@components': path.resolve(__dirname, 'src/components/'),
-      '@styles': path.resolve(__dirname, 'src/styles/'),
-    }
   },
   mode: 'production',
   module: {
